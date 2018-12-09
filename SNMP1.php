e2bec4422b48e971b80f9afd41422b12847dee2a
Tim <timweissenfels7@gmail.com>
2018-12-09 03:18:06 +0100

    Test Commit through storm

--- SNMP1.php
+++ SNMP1.php
@@ -17,42 +17,69 @@ function inset_val(&$Zaehler,&$conn) {
     $sql = "use drucker; INSERT INTO`test`(`Anzahl_Drucker`, `Name`, `ORT`)VALUES($Zaehler,'$Name[1]','$Ort[1]');";
 
     if ($conn->multi_query($sql) === TRUE) {
-        echo "\nHat funktioniert";
+        echo "\nZeile wurde hinzugefügt";
     } else {
-        echo "\nNe hat nicht" . $conn->error;
+        echo "\nNZeile wurde nicht hinzugefügt" . $conn->error;
     }
 
     return $Zaehler;
+}
+function get_conn_write($s_name,$u_name,$pwd) {
+    $conn_write = new mysqli($s_name, $u_name, $pwd);
+    if ($conn_write->connect_error) {
+        die("Connection failed: " . $conn_write->connect_error);
+    }
+    else
+        echo "\nVerbunden";
+
+    $sql_write = "use drucker;TRUNCATE TABLE `test`;";
+
+    if ($conn_write->multi_query($sql_write) === TRUE) {
+        echo "\nTabelle gelöscht";
+    } else {
+        echo "\nNe hat nicht gelöscht" . $conn_write->error;
+    }
+    $conn_write->next_result();
+    return $conn_write;
+}
+function get_conn_read($s_name,$u_name,$pwd){
+    $conn_read  = new mysqli($s_name, $u_name, $pwd);
+    if ($conn_read->connect_error) {
+        die("Connection failed: " . $conn_read->connect_error);
+    }
+    else
+        echo "\nVerbunden";
+
+    $sql_read  = "use drucker;";
+
+    $conn_read->query($sql_read);
+    if ($conn_read->query($sql_read) === TRUE) {
+        echo "\n";
+    } else {
+        echo "\nKonnte nicht gelöscht werden" . $conn_read->error;
+    }
+    $conn_read->next_result();
+    return $conn_read;
 }
 
 $servername = "localhost";
 $username = "root";
 $password = "";
-$Zaehler = 0;
+$zaehler = 0;
 
-$conn_write = new mysqli($servername, $username, $password);
-$conn_read  = new mysqli($servername, $username, $password);
-
-if (!$conn_write->connect_error && !$conn_read->connect_error) {
-    echo "Connected successfully";
-}
-else
-die("Connection failed: " . $conn_write->connect_error);
+$conn_write = get_conn_write($servername,$username,$password);
+$conn_read = get_conn_read($servername,$username,$password);
 
 
-$sql = "use drucker;TRUNCATE TABLE `test`;";
-
-if ($conn_write->multi_query($sql) === TRUE) {
-    echo "\nHat funktioniert";
-} else {
-    echo "\nNe hat nicht" . $conn_write->error;
-}
 $conn_write->next_result();
-inset_val($Zaehler,$conn_write);
-$conn_write->next_result();
-inset_val($Zaehler,$conn_write);
-$conn_write->next_result();
-inset_val($Zaehler,$conn_write);
+inset_val($zaehler,$conn_write);
 
+$sql_read  = "SELECT * FROM Abfrage;";
+
+$read_result_count_rows = $conn_read->query($sql_read);
+
+$zaehler = $read_result_count_rows->num_rows;
+
+echo "\n\n\n" . $zaehler;
 
 $conn_write->close();
